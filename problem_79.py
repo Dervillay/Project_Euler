75b7040fadb103fbb8c7d0bfa399e4ce2b3ee2f8
Dervillay <james.gascoigneburns7@gmail.com>
2021-06-19 16:24:18 +0100

    Add solution to problem 79

--- problem_79.py
+++ problem_79.py
@@ -4,19 +4,19 @@ with open('keylog.txt', 'r') as f:
 # Initial guess as concatenation of first two logins
 passcode = ['3', '1', '9', '6', '8', '0']
 
-for login in logins:
+for login_digits in logins:
 
-    if login[0] not in passcode:
-        passcode.insert(0, login[0])
-    first_digit_index = passcode.index(login[0])
+    if login_digits[0] not in passcode:
+        passcode.insert(0, login_digits[0])
+    first_digit_index = passcode.index(login_digits[0])
 
-    if login[1] not in passcode:
-        passcode.insert(first_digit_index + 1, login[1])
-    second_digit_index = passcode.index(login[1])
+    if login_digits[1] not in passcode:
+        passcode.insert(first_digit_index + 1, login_digits[1])
+    second_digit_index = passcode.index(login_digits[1])
 
-    if login[2] not in passcode:
-        passcode += login[2]
-    third_digit_index = passcode.index(login[2])
+    if login_digits[2] not in passcode:
+        passcode += login_digits[2]
+    third_digit_index = passcode.index(login_digits[2])
 
     if first_digit_index > third_digit_index:
         passcode[first_digit_index], passcode[third_digit_index] = passcode[third_digit_index], passcode[first_digit_index]
